7004b102f53790d418ff2552edaa87dc133f1e16
Endi Suwandi <endisuwandi64@gmail.com>
2025-07-31 17:11:24 +0700

    refactor: adjust stylesheet import to match renamed file Globals.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./Globals.css";
 
 createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
