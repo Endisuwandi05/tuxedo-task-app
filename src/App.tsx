02e3728e4de6c2a78ab09bd4cf8cd6fb4d80b879
M Haidar Hanif <git@mhaidarhanif.com>
2025-08-02 20:25:50 +0700

    task component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 const dataTasks = [
-  { id: 1, name: "Breakfast" },
-  { id: 2, name: "Jogging" },
-  { id: 3, name: "Study" },
+  { id: 1, name: "Breakfast", isCompleted: true },
+  { id: 2, name: "Jogging", isCompleted: false },
+  { id: 3, name: "Study", isCompleted: false },
 ];
 
 export function App() {
@@ -11,11 +11,27 @@ export function App() {
         {dataTasks.map((task) => {
           return (
             <li key={task.id}>
-              <p>{task.name}</p>
+              <Task name={task.name} isCompleted={task.isCompleted} />
             </li>
           );
         })}
       </ul>
+    </div>
+  );
+}
+
+export function Task({
+  name,
+  isCompleted,
+}: {
+  name: string;
+  isCompleted: boolean;
+}) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      {isCompleted && <p>Completed ✅</p>}
+      {!isCompleted && <p>Incomplete ❌</p>}
     </div>
   );
 }
