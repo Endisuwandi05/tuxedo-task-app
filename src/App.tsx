d7f1eec7311381cdaf727e44dd5251ed164c1ecc
M Haidar Hanif <git@mhaidarhanif.com>
2025-08-09 20:29:50 +0700

    feat: TaskList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import TaskItem from "./components/TaskItem";
-
-const dataTasks = [
-  { id: 1, name: "Belajar React", isCompleted: true },
-  { id: 2, name: "Selesaikan Proyek Figma", isCompleted: true },
-  { id: 3, name: "Makan Siang", isCompleted: false },
-  { id: 4, name: "Makan malam", isCompleted: true },
-];
+import { TaskList } from "./components/TaskList";
 
 function App() {
   return (
     <div className="p-10 space-y-10">
-      <h1 className="text-3xl font-bold">Daftar Tugas</h1>
+      <h1 className="text-3xl font-bold">Tuxedo: Daftar Tugas</h1>
 
-      <ul>
-        {dataTasks.map((task) => {
-          return (
-            <li key={task.id}>
-              <TaskItem name="Belajar React" isCompleted={true} />
-            </li>
-          );
-        })}
-      </ul>
+      <TaskList />
     </div>
   );
 }
