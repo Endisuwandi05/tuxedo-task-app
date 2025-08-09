2767d5c69b5eed0df4af6c9d2d4602144dddfab2
M Haidar Hanif <git@mhaidarhanif.com>
2025-08-09 20:23:23 +0700

    refactor: use dataTasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import TaskItem from "./components/TaskItem";
 
+const dataTasks = [
+  { id: 1, name: "Belajar React", isCompleted: true },
+  { id: 2, name: "Selesaikan Proyek Figma", isCompleted: true },
+  { id: 3, name: "Makan Siang", isCompleted: false },
+  { id: 4, name: "Makan malam", isCompleted: true },
+];
+
 function App() {
   return (
     <div className="App">
       <h1>Daftar Tugas</h1>
 
-      <TaskItem name="Belajar React" isCompleted={true} />
-      <TaskItem name="Selesaikan Proyek Figma" isCompleted={true} />
-      <TaskItem name="Makan Siang" isCompleted={false} />
-      <TaskItem name="Makan malam" isCompleted={true} />
+      <ul>
+        {dataTasks.map((task) => {
+          return (
+            <li key={task.id}>
+              <TaskItem name="Belajar React" isCompleted={true} />
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
