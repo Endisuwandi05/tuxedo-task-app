39319a8299401aca9ed01b8914b2b600517049df
endisuwandi <endisuwandi64@gmail.com>
2025-08-16 20:01:49 +0700

    feat add input

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
     <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold mb-6 text-center"> Tuxedo : Tugas  Saya</h1>
       <div className="w-full max-w-lg p-6 bg-white shadow-lg rounded-lg">
+        <form action=""></form>
         <TaskList />
       </div>
     </div>
