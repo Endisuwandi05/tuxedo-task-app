b97b7c2bfc714dbbca02ac1d43651ad65464c9de
endisuwandi <endisuwandi64@gmail.com>
2025-08-19 15:29:05 +0700

    - Remove unnecessary whitespace inside the <h1> tag.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { TaskList } from './components/TaskList';
+import { TaskList } from "./components/TaskList";
 
 function App() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold mb-6 text-center"> 
+      <h1 className="text-4xl font-bold mb-6 text-center">
         {" "}
-        
-        Tuxedo : Tugas  Saya</h1>
+        Tuxedo : Tugas Saya
+      </h1>
       <div className="w-full max-w-lg p-6 bg-white shadow-lg rounded-lg">
-      
         <TaskList />
       </div>
     </div>
