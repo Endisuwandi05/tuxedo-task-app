b3411edcfd814036f507078795a72be5b328c1a8
endisuwandi <endisuwandi64@gmail.com>
2025-08-19 16:12:23 +0700

    menambahkan fitur baru yaitu tasks

---
+++ src/data/Tasks.tsx
@@ -0,0 +1,7 @@
+export const initialDataTasks = [
+  { id: 1, name: "Belajar React", isCompleted: true },
+  { id: 2, name: "Selesaikan Proyek Figma", isCompleted: true },
+  { id: 3, name: "Makan Siang", isCompleted: false },
+  { id: 4, name: "Makan Malam", isCompleted: true },
+  { id: 5, name: "Makan Bubur", isCompleted: true },
+];
