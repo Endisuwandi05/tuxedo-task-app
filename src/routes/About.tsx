159c71b8b3546a619b24a82952db178051ccd8ac
endisuwandi <endisuwandi64@gmail.com>
2025-08-19 20:42:18 +0700

    refactor code and add new tag p and lorem paragraf to About file

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,7 +1,11 @@
 export function About() {
   return (
     <div className="m-10">
-      <h1 className="text-2x1 font-bold">About </h1>
+      <h1 className="text-2x1 font-bold">Tuxedo</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim ex
+        facilis suscipit, asperiores natus hic id laborum eos vel commodi.
+      </p>
     </div>
   );
 }
