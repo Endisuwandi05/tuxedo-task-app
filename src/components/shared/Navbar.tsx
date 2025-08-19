0be18168f6f622f62f15400516aaa20cf6ced493
endisuwandi <endisuwandi64@gmail.com>
2025-08-19 20:32:11 +0700

    feat add navbar route

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,11 @@
+export function Navbar() {
+  return (
+    <nav>
+      <h1>Tuxedo</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import App from "./App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
 import { About } from "./routes/About.tsx";
+import { Navbar } from "./components/shared/Navbar.tsx";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
+    <Navbar />
     <Routes>
       <Route index element={<App />} />
       <Route path="about" element={<About />} />
