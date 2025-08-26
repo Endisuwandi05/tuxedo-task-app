23bf4c6e0330ac5e781cab1d8ab3149aa3317a5a
endisuwandi <endisuwandi64@gmail.com>
2025-08-26 20:05:17 +0700

    fix style link hover

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -5,12 +5,19 @@ export function Navbar() {
     <nav className="flex flex-col w-64 p-4 border-r border-gray-200">
       <h2 className="text-xl font-semibold mb-4">Tuxedo App</h2>
       <ul className="flex flex-col gap-y-2">
-        <li className="px-4 py-2 hover:bg-gray-200 rounded-lg transition-colors">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="px-4 py-2 hover:bg-gray-200 rounded-lg transition-colors">
-          <Link to="/memulai">Start</Link>
-        </li>
+        <Link
+          to="/"
+          className="px-4 py-2 hover:bg-gray-200 rounded-lg transition-colors"
+        >
+          Home
+        </Link>
+
+        <Link
+          to="/memulai"
+          className="px-4 py-2 hover:bg-gray-200 rounded-lg transition-colors"
+        >
+          Start
+        </Link>
       </ul>
     </nav>
   );
