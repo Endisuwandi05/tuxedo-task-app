811fd635dc8b8f8a7d342e8bfd343bbea82e6adc
endisuwandi <endisuwandi64@gmail.com>
2025-08-26 15:25:56 +0700

    chore: Clear sample data

--- src/data/Tasks.tsx
+++ src/data/Tasks.tsx
@@ -1,7 +1,1 @@
-export const initialDataTasks = [
-  { id: 1, name: "Belajar React", isCompleted: false },
-  { id: 2, name: "Selesaikan Proyek Figma", isCompleted: false },
-  { id: 3, name: "Makan Siang", isCompleted: false },
-  { id: 4, name: "Makan Malam", isCompleted: false },
-  { id: 5, name: "Makan Bubur", isCompleted: false },
-];
+export const initialDataTasks = [];
